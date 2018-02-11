59ad9f316bbf62c707b31f58e75f546ba9386baa
me-go <monigkeit1@cox.net>
2018-02-10 17:53:08 -0700

    Remove unnecessary using

--- InventoryCoreVisualStudio/ViewModels/ItemEditViewModel.cs
+++ InventoryCoreVisualStudio/ViewModels/ItemEditViewModel.cs
@@ -2,7 +2,6 @@
 using Microsoft.AspNetCore.Mvc.Rendering;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
